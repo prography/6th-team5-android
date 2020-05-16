15cbbf7635b7a59264dfab8ee513af03a871c3e5
d2634 <59246542+d2634@users.noreply.github.com>
2020-05-16 22:17:39 +0900

    make recyclerview
    make recyclerview

--- skycastle/app/src/main/java/com/example/skycastle/SelectUnivPage.java
+++ skycastle/app/src/main/java/com/example/skycastle/SelectUnivPage.java
@@ -1,15 +1,49 @@
 package com.example.skycastle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.view.WindowManager;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SelectUnivPage extends AppCompatActivity {
+    RecyclerView recyclerView;
+    recyclerAdapter adapter;
+    GridLayoutManager layoutManager;
+
+    ArrayList<UnivList> list = new ArrayList<UnivList>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_univ_page);
+
+        recyclerView = (RecyclerView)findViewById(R.id.univ_recycler_view);
+        adapter = new recyclerAdapter(getApplicationContext(), list);
+
+        layoutManager = new GridLayoutManager(getApplicationContext(), 2);
+
+        layoutManager.setSpanSizeLookup(new GridLayoutManager.SpanSizeLookup() {
+            @Override
+            public int getSpanSize(int position) {
+                return 1;
+            }
+        });
+
+        //임시로 데이터 추가
+        String[] n_list = {"서울대학교","연세대학교","고려대학교","서강대학교","성균관대학교","한양대학교","중앙대학교","경희대학교","숙명여자대학교"};
+        for(int i=0;i<n_list.length;i++){
+            UnivList univ = new UnivList();
+            univ.setName(n_list[i]);
+            list.add(univ);
+        }
+
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setAdapter(adapter);
+
     }
 }
---
+++ skycastle/app/src/main/java/com/example/skycastle/recyclerAdapter.java
@@ -0,0 +1,86 @@
+package com.example.skycastle;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.util.SparseBooleanArray;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class recyclerAdapter extends RecyclerView.Adapter<recyclerAdapter.ItemViewHolder>{
+    private List<UnivList> listData;
+    private Context context;
+    private SparseBooleanArray selectedItems = new SparseBooleanArray();
+    private ItemViewHolder holder;
+    private  View view;
+    private int position;
+
+    public recyclerAdapter(Context context, List<UnivList> list){
+        this.context = (Context) context;
+        this.listData = list;
+    }
+    @NonNull
+    @Override
+    public ItemViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        context = parent.getContext();
+        view = LayoutInflater.from(parent.getContext()).inflate(R.layout.univlist_recycler, parent, false);
+        return new ItemViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull recyclerAdapter.ItemViewHolder holder, int position) {
+        holder.onBind(listData.get(position),position);
+    }
+
+    @Override
+    public int getItemCount() {
+        return listData.size();
+    }
+
+    void addItem(UnivList data) {
+        // 외부에서 item을 추가시킬 함수입니다.
+        listData.add(data);
+    }
+
+    public class ItemViewHolder extends RecyclerView.ViewHolder {
+        private TextView textView;
+        private ImageView u_icon;
+        private ConstraintLayout list_con;
+        private  UnivList data;
+        private int position;
+
+        public ItemViewHolder(@NonNull View itemView) {
+            super(itemView);
+            textView = itemView.findViewById(R.id.u_name);
+            u_icon = itemView.findViewById(R.id.u_image);
+            list_con = itemView.findViewById(R.id.list_con);
+            list_con.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int pos = getAdapterPosition() ;
+                    //Intent intent=new Intent(Intent.ACTION_VIEW, Uri.parse(listData.get(pos).getUrl()));
+                    //context.startActivity(intent);
+                }
+            });
+        }
+
+        public void onBind(UnivList univList, int position) {
+            this.data = univList;
+            this.position = position;
+            final String name = data.getName();
+            final String image = data.getLogo();
+
+            textView.setText(name);
+            //u_icon.set
+        }
+    }
+}
